288b0ab89157546ea3fc270a66133a69d2bc63f4
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-10-12 06:37:18 +0300

    algorithms

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,41 +1,49 @@
-nclude "sort.h"
-
+#include "sort.h"
 /**
-*swap - the positions of two elements into an array
-*@array: array
-*@item1: array element
-*@item2: array element
-*/
-void swap(int *array, int item1, int item2)
+ * knuth_gap - gets the gap size for the sort
+ * @size: size of array
+ *
+ * Return: the gap size
+ */
+size_t knuth_gap(size_t size)
 {
+	/* declarations */
+	size_t gap = 1;
 
-	int tmp;
-
-	tmp = array[item1];
-	array[item1] = array[item2];
-	array[item2] = tmp;
+	/* looking for gap */
+	while (gap < size)
+		gap = gap * 3 + 1;
+	return ((gap - 1) / 3);
 }
 /**
- * shell_sort - function that sorts an array of integers in ascending
- * order using the Shell sort algorithm, using the Knuth sequence
- * @size: size of the array
- * @array: list with numbers
+ * shell_sort - sort an int array via shell sort
+ * @array: the array
+ * @size: the size of the array
+ *
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, i, index = 0;
+	/* declarations */
+	size_t gap, i, j;
+	int temp;
 
-	if (array == NULL || size < 2)
+	/* check for bad stuff */
+	if (!(array && size))
 		return;
-	while (gap < size / 3)
-		gap = 3 * gap + 1;
-	while (gap >= 1)
+	/* do the sorting and printing */
+	gap = knuth_gap(size);
+	while (gap)
 	{
 		for (i = gap; i < size; i++)
-			for (index = i; index >= gap &&
-			 (array[index] < array[index - gap]); index -= gap)
-				swap(array, index, index - gap);
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
 		print_array(array, size);
-		gap /= 3;
+		gap = gap / 3;
 	}
 }
